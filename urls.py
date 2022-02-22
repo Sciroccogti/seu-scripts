768edd49eb801c451e7a91030556b44a597ad571
Sciroccogti <scirocco_gti@yeah.net>
2022-02-22 16:28:00 +0800

    update res_val_image url

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-res_val_image = 'http://yuyue.seu.edu.cn/eduplus/control/validateimage'
+res_val_image = 'http://yuyue.seu.edu.cn:80/eduplus/validateimage'
 res_url = 'http://yuyue.seu.edu.cn/eduplus/order/order/order/insertOredr.do?sclId=1'
 
 login_home = 'https://newids.seu.edu.cn/authserver/login?goto=http://my.seu.edu.cn/index.portal'
